6c96c5ba3eec76e039d44ed25a7ff5d17c381603
aaron-nls <aaron@nlsltd.com>
2022-10-20 00:14:33 +0100

    finished!

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,13 @@
 const player = document.getElementById('user');
 const video = document.getElementById('video');
 const bg = document.getElementById('bg');
+const winnie = document.getElementById('winnie');
+const shade = document.getElementById('shade');
+const scream = document.getElementById('scream');
+const logo = document.getElementById('logo');
+const flame = document.getElementById('flame');
 
+let volumeInterval = null;
 
 $(document).ready(function(){
   video.play();
@@ -10,72 +16,121 @@ $(document).ready(function(){
   $('[href^="#"]').click(function(){
     changePage($(this).attr('href'));
   });
-  // $('.js-enterFullScreen').click(function(){
-  //   enterFullScreen();
-  // });
 
-  
-  (async () => {
-    let volumeCallback = null;
-    let volumeInterval = null;
-    
-    // Initialize
-    try {
-      const audioStream = await navigator.mediaDevices.getUserMedia({
-        audio: {
-          echoCancellation: true
-        }
-      });
-      const audioContext = new AudioContext();
-      const audioSource = audioContext.createMediaStreamSource(audioStream);
-      const analyser = audioContext.createAnalyser();
-      analyser.fftSize = 512;
-      analyser.minDecibels = -127;
-      analyser.maxDecibels = 0;
-      analyser.smoothingTimeConstant = 0.4;
-      audioSource.connect(analyser);
-      const volumes = new Uint8Array(analyser.frequencyBinCount);
-      volumeCallback = () => {
-        analyser.getByteFrequencyData(volumes);
-        let volumeSum = 0;
-        for(const volume of volumes)
-          volumeSum += volume;
-        const averageVolume = volumeSum / volumes.length;
-        // Value range: 127 = analyser.maxDecibels - analyser.minDecibels;
-        console.log(averageVolume * 100 / 127);
-        //volumeVisualizer.style.setProperty('--volume', (averageVolume * 100 / 127) + '%');
-      };
-    } catch(e) {
-      console.error('Failed to initialize volume visualizer, simulating instead...', e);
+  $('.fire').click(function(){
+    if(!$('body').hasClass('black-flame')){
+      winnie.currentTime = 0;
+      winnie.play();
     }
-    // Use
-    $('.js-enterFullScreen').click(function(){
-      // Updating every 100ms (should be same as CSS transition speed)
-      video.muted =false;
-      if(volumeCallback !== null && volumeInterval === null)
-        volumeInterval = setInterval(volumeCallback, 100);
-      
-      enterFullScreen();
-    });
-    // stopButton.addEventListener('click', () => {
-    //   if(volumeInterval !== null) {
-    //     clearInterval(volumeInterval);
-    //     volumeInterval = null;
-    //   }
-    // });
-  })();
+    $('body').toggleClass('black-flame');
+  });
 
+  winnie.onended = function() {
+    $('body').removeClass('black-flame');
+  };
+  
+  $('#logo').click(function(){
+    $(this).addClass('animate__bounce');
+    shade.currentTime = 0;
+    shade.play();
 });
+$('#queen').click(function(){
+    changePage('#home');
+});
+
+    
+  logo.addEventListener('animationend', () => {
+    $(logo).removeClass('animate__bounce');
+  });
+});
+  
 
 function changePage(pageID){
   $('.pages section').removeClass('visible');
   $('.pages ' + pageID).addClass('visible');
+  $('body').removeClass('black-flame');
+  $('html,body').scrollTop(0);
   window.navigator.vibrate([300, 100, 200, 50, 300]);
+  if(pageID == '#quiet') {
+    $('body').addClass('quiet');
+    video.muted = true;
+    doBlow();
+  }else{
+    stopBlow();
+    $('body').removeClass('extinguished quiet');
+
+    video.muted = false;
+  }
+}
+
+
+function stopBlow() {
+  if(volumeInterval !== null) {
+    audioContext = null;
+    audioSource = null;
+    analyser = null;
+    volumes = null;
+    audioStream.getTracks().forEach(function(track) {
+      track.stop();
+    });
+    clearInterval(volumeInterval);
+  }
+}
+
+let audioContext = null;
+let audioSource = null;
+let analyser = null;
+let volumes = null;
+let audioStream = null;
+async function doBlow() {
+    
+  try {
+    audioStream = await navigator.mediaDevices.getUserMedia({
+      audio: {
+        echoCancellation: true
+      }
+    });
+
+    audioContext = new AudioContext();
+    audioSource = audioContext.createMediaStreamSource(audioStream);
+    analyser = audioContext.createAnalyser();
+    analyser.fftSize = 512;
+    analyser.minDecibels = -127;
+    analyser.maxDecibels = 0;
+    analyser.smoothingTimeConstant = 0.4;
+    audioSource.connect(analyser);
+    volumes = new Uint8Array(analyser.frequencyBinCount);
+    
+    if(volumeCallback !== null && volumeInterval === null){
+      volumeInterval = setInterval(volumeCallback, 100);
+    }
+  } catch(e) {
+    alert('Soz, no mic, no blow!');
+  }
+}
+
+ 
+function volumeCallback()  {
+  analyser.getByteFrequencyData(volumes);
+  let volumeSum = 0;
+  for(const volume of volumes)
+    volumeSum += volume;
+  const averageVolume = volumeSum / volumes.length;
+  // Value range: 127 = analyser.maxDecibels - analyser.minDecibels;
+  let vol = (averageVolume * 100 / 127);
+  console.log(vol);
+  flame.style.setProperty('--scale', 1 - (averageVolume / 127) + '');
+  if(vol > 70) {
+    stopBlow();
+    $('body').addClass('extinguished');
+    scream.currentTime = 0;
+    scream.play();
+    $('#quiettime').remove();
+  }
 }
 
 
 function enterFullScreen() {
-  audio.play();
   changePage('#home');
 }
 
