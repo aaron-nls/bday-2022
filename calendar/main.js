350067bbe7fc8e4c8b682a54ce906afd0dca881a
Aaron <aarono@outlook.com>
2023-11-05 18:42:57 +0000

    Add files via upload

--- calendar/main.js
+++ calendar/main.js
@@ -4,6 +4,64 @@ $(document).ready(function() {
         var target = $(this).attr('data-page');
         goToPage(target);
     });
+    $('[data-show]').click(function() {
+        var target = $(this).attr('data-show');
+        showElement(target);
+    });
+    $('[data-toggle]').click(function() {
+        var target = $(this).attr('data-toggle');
+        toggleElement(target);
+    });
+    $('[data-hide]').click(function() {
+        var target = $(this).attr('data-hide');
+        hideElement(target);
+    });
+    $('[data-enable]').click(function() {
+        var target = $(this).attr('data-enable');
+        enableElement(target);
+    });
+    $('[data-run]').click(function() {
+        var target = $(this).attr('data-run');
+        window[target]();
+    });
+    $('[data-sound-fx]').click(function() {
+        var target = $(this).attr('data-sound-fx');
+        var enabled = $(this).attr('data-enabled') ?? 'true';
+        if(enabled === 'true') {
+            playSound(target ?? null);
+        }
+    });
+
+
+    var dragStartX, initialLeft;
+    var draggable = null;
+    $('[data-drag-x]').on('touchstart', function(event) {
+        event.preventDefault();
+        console.log('touchstart');
+        draggable = this;
+        var clientX = event.clientX || event.touches[0].clientX;
+        var rect = draggable.getBoundingClientRect();
+        dragStartX = clientX;
+        initialLeft = rect.left;
+
+        document.addEventListener('mousemove', onMove);
+        document.addEventListener('touchmove', onMove);
+        document.addEventListener('mouseup', onEnd);
+        document.addEventListener('touchend', onEnd);
+    });
+
+    function onMove(event) {
+        var clientX = event.clientX || event.touches[0].clientX;
+        var newLeft = initialLeft + clientX - dragStartX;
+        draggable.style.transform = `translateX(${newLeft}px)`;
+    }
+    
+    function onEnd() {
+        document.removeEventListener('mousemove', onMove);
+        document.removeEventListener('touchmove', onMove);
+        document.removeEventListener('mouseup', onEnd);
+        document.removeEventListener('touchend', onEnd);
+    }
 
 });
 
@@ -38,14 +96,14 @@ function playBackground(newBgAudio, bgVolume) {
 
     if(newBgAudio === null) {
         bgAudio = null;
-        bgAudioPlayer.pause();
+        if(bgAudioPlayer) bgAudioPlayer.pause();
     }else{
         bgAudioPlayer.volume = bgVolume ?? 0.5;
     }
 }
 
 function playSound(newSoundAudio) {
-    if(newSoundAudio && newSoundAudio !== soundAudio) {
+    if(newSoundAudio) {
         soundAudio = newSoundAudio;
         if(soundAudioPlayer) {
             soundAudioPlayer.pause();
@@ -56,6 +114,82 @@ function playSound(newSoundAudio) {
 
     if(newSoundAudio === null) {
         soundAudio = null;
-        soundAudioPlayer.pause();
+        if(soundAudioPlayer) soundAudioPlayer.pause();
+    }
+}
+
+function showElement(element) {
+    $('page:visible [data-id="' + element + '"]').fadeIn();
+}
+
+function hideElement(element) {
+    $('page:visible [data-id="' + element + '"]').fadeOut();
+}
+
+function toggleElement(element) {
+    $('page:visible [data-id="' + element + '"]').fadeToggle();
+}
+
+function enableElement(element) {
+    $('page:visible [data-id="' + element + '"]').attr('data-enabled', 'true');
+}
+
+let blurTime;
+function windowFocus() {
+    window.addEventListener("focus", function(event) {
+        const focusTime = new Date();
+        const elapsedSeconds = (focusTime - blurTime) / 1000;
+        if (elapsedSeconds >= 10) {
+            enableElement('present-button');
+            hideElement('paper-button');
+            hideElement('note');
+        }
+    }, false);
+
+    window.addEventListener("blur", function(event) {
+        blurTime = new Date();
+    }, false);
+}
+
+function onDragStart(event) {
+  }
+
+function waterCycle() {
+
+    if(requestGyroscopeAccess()) {
+        window.addEventListener('deviceorientation', (event) => { 
+            var alpha = event.alpha; // rotation around z-axis (-180 to 180)
+            var beta = event.beta; // front to back motion (-180 to 180)
+            var gamma = event.gamma; // left to right motion (-90 to 90)
+    
+            console.log('alpha: ' + alpha);
+            console.log('beta: ' + beta);
+            console.log('gamma: ' + gamma);
+            $('#door2 .water').style.transform = `rotateX(${gamme}deg)`;
+        });
+    }else{
+        $('#door2 .water').addClass('a-rotating');
+    }
+}
+
+function requestGyroscopeAccess() {
+    if (window.DeviceOrientationEvent) {
+        if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+            DeviceOrientationEvent.requestPermission()
+                .then(permissionState => {
+                    if (permissionState === 'granted') {
+                        return true;
+                    }else{
+                        return false;
+                    }
+                })
+                .catch();
+        } else {
+            // handle regular non iOS 13+ devices
+            return true;
+        }
+    } else {
+        console.log('This browser does not support gyroscope functionality.');
+        return false;
     }
 }
